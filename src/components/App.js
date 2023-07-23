6a2f15e1f6a2448e5632d224173763a7ec2b0a57
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-07-23 19:19:29 +0200

    add tweets

--- src/components/App.js
+++ src/components/App.js
@@ -175,7 +175,29 @@ function App() {
   };
   const renderTweets = () => {
     return tweets.map((tweet) => {
-      return <li></li>;
+      return (
+        <li key={tweet.id}>
+          <article className="tweet__wrapper">
+            <img className="tweet__avatar" src={tweet.avatar} alt="photo" />
+            <div className="tweet__content">
+              <p className="tweet__info">
+                <span className="tweet__user">{tweet.user}</span>
+                <span className="tweet__username">@{tweet.username}</span>
+                <span className="tweet__date">{tweet.date}</span>
+              </p>
+              <p className="tweet__text">{tweet.text}</p>
+              <ul className="tweet__actions">
+                <li className="tweet__comments">{tweet.comments}</li>
+                <li className="tweet__retweets">{tweet.retweets}</li>
+                <li className="tweet__likes">{tweet.likes}</li>
+                <li className="tweet__share">
+                  <span className="tweet__share--text">Compartir</span>
+                </li>
+              </ul>
+            </div>
+          </article>
+        </li>
+      );
     });
   };
   return (
